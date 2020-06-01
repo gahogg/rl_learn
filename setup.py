c87ac4f852e397aab76fcac839fdebcc356f2e17
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2020-06-01 15:08:15 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
   name='rl_learn',
-  version='0.1',
+  version='0.1.5',
   description='A package to accompany the book "Reinforcement Learning: A Python Introduction"',
+  long_description="rl-learn is a Python package to accompany \"Reinforcement Learning: A Python Introduction\", which is a book that teaches fundamental reinforcement learning techniques in Python. It closely follows Sutton and Barto's famous \"Reinforcement Learning: An Introduction\".",
   author='Gregory Hogg',
   author_email='gahogg@uwaterloo.ca',
-  url='https://github.com/gahogg/rl_learn.git/dist/rl_learn-0.1.tar.gz',
+  url='https://github.com/gahogg/rl_learn.git',
   license='MIT',
-  packages=['rl_learn'],
+  packages=['rl_learn', 'rl_learn/bandits'],
   zip_safe=False)
