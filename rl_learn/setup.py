2c0977ca489b03535f5d5c1aeb580f50b6eaef8c
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2020-11-16 11:37:32 -0500

    Fixed dependencies for setup

--- rl_learn/setup.py
+++ rl_learn/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='rl_learn',
-  version='1.0.1',
+  version='1.0.2',
   description='A package to accompany the book "Reinforcement Learning: A Python Introduction"',
   long_description="rl-learn is a Python package that teaches fundamental reinforcement learning techniques in Python. It closely follows Sutton and Barto's famous \"Reinforcement Learning: An Introduction\".\nCheck out the GitHub for example code: https://github.com/gahogg/rl_learn",
   author='Gregory Hogg',
@@ -10,5 +10,5 @@ setup(
   url='https://github.com/gahogg/rl_learn.git',
   license='MIT',
   packages=['rl_learn', 'rl_learn/bandits'],
-  install_requires=["numpy>=1.18.1", "matplotlib>=3.1.3"],
+  install_requires=["numpy>=1.18.1,<=1.18.1", "matplotlib>=3.1.3,<=3.1.3"],
   zip_safe=False)
